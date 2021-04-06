695508d00cbe45c67ba5350126677924b55d0bda
VictorFHernandez <victorfhernandez3@gmail.com>
2021-04-06 12:24:39 -0500

    completed the jQuery mouse event exercise

--- js/jquery_exercises.js
+++ js/jquery_exercises.js
@@ -1,8 +1,21 @@
 "use strict";
 (function() {
+    $('h1').css('background-color', 'rebeccapurple')
+    $('h1').click(function() {
+        $(this).css('background-color', 'yellow')
+    });
 
-
-
+    $('p').dblclick(function() {
+        $(this).css('font-size', '18px')
+    })
+    $('li').hover(
+        function() {
+            $(this).css('background-color', '#FF0');
+        },
+        function() {
+            $(this).css('background-color', '#FFF');
+        }
+    );
 
 
 
